63b5e89b232954ac889fdc7823d5b94bb8ec8a68
karololszanski <me@karololszanski.com>
2022-02-10 21:13:36 +0100

    Small design fix for chart tile (mobile)

--- app/components/elements/tile/tile.styles.ts
+++ app/components/elements/tile/tile.styles.ts
@@ -2,8 +2,6 @@ import theme from "styles/default";
 
 export const TileStyles: any = {
   display: "flex",
-  minHeight: "400px",
-  maxWidth: "600px",
   backgroundColor: "white",
   borderRadius: "12px",
   position: "relative",
